b22e69b1dfeb59844ec8e1ac834108b6e00f85a1
Joao Sequeira <joaosequeira28@hotmail.com>
2022-09-24 16:56:24 +0100

    MUDANCA NA PORTA DO BACKOFFICE
    Falar com alex devido ao ficheiro env

--- website/src/App.js
+++ website/src/App.js
@@ -1,20 +1,10 @@
-import './styles/main.css'
+import { BrowserRouter as Router, Route, Routes, Navigate, useNavigate } from 'react-router-dom'
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Teste</p>
       </header>
     </div>
   );
