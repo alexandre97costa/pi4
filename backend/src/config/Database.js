14b2408ff7017009392c3dfb5cd4f63c16c0f232
Joao Sequeira <joaosequeira28@hotmail.com>
2022-09-30 10:15:36 +0100

    mudanca

--- backend/src/config/Database.js
+++ backend/src/config/Database.js
@@ -1,19 +1,39 @@
-var Sequelize = require("sequelize");
+const { Sequelize } = require('sequelize');
+
+// todo: Github Action para alojar no heroku
 
 const sequelize = new Sequelize(
-    'database',
-    'username', 
-    'password',
+    'adm-test-database',    // nome da db
+    'postgres',         // user
+    'postgres',         // pass
     {
-        host: '',
-        port: "5432",
-        dialect: "postgres",
-        dialectOptions: {
-            ssl:{
-                rejectUnauthorized: false
+        host: 'localhost',
+        port: '5432',
+        dialect: 'postgres',
+        logging: false,
+        define: {
+            // hooks globais, atingem todos os modelos
+            hooks: {
+                afterCreate: model => {
+                    const ignoreModels = [
+                        // mete aqui as tabelas que NÃO queres ter a aparecer na consola quando são criadas.
+                        'uma-cena-qualquer'
+                    ]
+                    if (!ignoreModels.includes(model.constructor.name)) {
+                        console.log('\x1b[37m\x1b[46m ' + model.constructor.name + '(#' + model.id + ') criado \x1b[0m ')
+                    }
+                },
+                afterUpdate: model => {
+                    console.log('\x1b[37m\x1b[43m ' + model.constructor.name + '(#' + model.id + ') atualizado \x1b[0m ')
+                },
+                afterDestroy: model => {
+                    console.log('\x1b[37m\x1b[41m ' + model.constructor.name + '(#' + model.id + ') eliminado ⚠ \x1b[0m ')
+                }
             }
         }
     }
-);
+)
 
-module.exports = sequelize;
+sequelize.sync()
+
+module.exports = sequelize 
