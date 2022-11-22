0ac07b4f8a0ec9ab9685dcd0d5fe73993715ddec
Joao Sequeira <joaosequeira28@hotmail.com>
2022-11-22 22:51:44 +0000

    NAO SEI

---
+++ app/app/src/main/java/pi4/main/MainActivity2.kt
@@ -0,0 +1,11 @@
+package pi4.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
