8651b1143a90236e10682060f8b78f1de35b7611
Raquelmeida <95857867+Raquelmeida@users.noreply.github.com>
2022-11-23 22:16:08 +0000

    conteudo da pagina gerir microsite
    criacao do conteudo para a conteudo da pagina gerir microsite

--- backoffice/src/Pages/GerirMicrosite/GerirMicrosite/gerirMicrosite.js
+++ backoffice/src/Pages/GerirMicrosite/GerirMicrosite/gerirMicrosite.js
@@ -1,45 +1,77 @@
 import React from 'react';
 
-export default function GerirMicrosite(){
-return(
-    <div className='row'>
-        <div className='col-12'>
+export default function GerirMicrosite() {
+    return (
 
-            <div class="card-group">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+
+        <div className='row gx-3'>
+            <div className='col-12'>
+                <p className='fs-3 mb-5'> Gerir Microsite</p>
+
+                <div className='row'>
+                    <div className='col-12'>
+                        <div className='row card-group'>
+                            <div className='col-12 card m-2 shadow'>
+                                <div className='card-body'>
+                                    <h5 className='card-title text-center'>Menu</h5>
+                                    <p className='card-text text-center'><small className='text-muted'>Cabeçalho</small></p>
+                                </div>
+                            </div>
+
+                            <div className=' col-12 card m-2 shadow'>
+                                <div className='card-body'>
+                                    <h5 className='card-title text-center'>Hero Banner</h5>
+                                    <p className='card-text text-center'><small className='text-muted'>Card 0</small></p>
+                                </div>
+                            </div>
+
+                            <div className=' col-12 card m-2 shadow'>
+                                <div className='card-body'>
+                                    <h5 className='card-title text-center'>Vantagens App</h5>
+                                    <p className='card-text text-center'><small className='text-muted'>Card </small></p>
+                                </div>
+                            </div>
+
+                            <div className='col-12 card m-2 shadow'>
+                                <div className='card-body '>
+                                    <h5 className='card-title text-center'>Publicidade Agente Turístico</h5>
+                                    <p className='card-text text-center'><small className='text-muted'>Card 2</small></p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className='row'>
+                    <div className='col-12'>
+                        <div className='row card-group'>
+                            <div className='col-12 card m-2 shadow'>
+                                <div className='card-body'>
+                                    <h5 className='card-title text-center'>Descarregar App</h5>
+                                    <p className='card-text text-center'><small className='text-muted'>Card 3</small></p>
+                                </div>
+                            </div>
+
+                            <div className=' col-12 card m-2 shadow'>
+                                <div className='card-body'>
+                                    <h5 className='card-title text-center'>Footer</h5>
+                                    <p className='card-text text-center'><small className='text-muted'>Rodapé</small></p>
+                                </div>
+                            </div>
+
+                            <div className=' col-12 card m-2 shadow'>
+                                <div className='card-body'>
+                                    <h5 className='card-title text-center'>+</h5>
+                                    <p className='card-text text-center'><small className='text-muted'>Adicionar Conteudo</small></p>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>    
-
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>    
             </div>
         </div>
-    </div>
-
-);
-    
+    );
 }
