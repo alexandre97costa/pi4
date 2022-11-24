821f344c6fb50e1250c2b7e60b2074273290f31a
Raquelmeida <95857867+Raquelmeida@users.noreply.github.com>
2022-11-24 22:53:08 +0000

    Update gerirMicrosite.js

--- backoffice/src/Pages/GerirMicrosite/GerirMicrosite/gerirMicrosite.js
+++ backoffice/src/Pages/GerirMicrosite/GerirMicrosite/gerirMicrosite.js
@@ -9,28 +9,28 @@ export default function GerirMicrosite() {
                 <div className='row'>
                     <div className='col-12'>
                         <div className='row card-group'>
-                            <div className='col-12 card m-2 shadow'>
+                            <div className='col-12 card  p-3 m-2 shadow'>
                                 <div className='card-body'>
                                     <h5 className='card-title text-center'>Menu</h5>
                                     <p className='card-text text-center'><small className='text-muted'>Cabeçalho</small></p>
                                 </div>
                             </div>
 
-                            <div className=' col-12 card m-2 shadow'>
+                            <div className=' col-12 card p-3  m-2 shadow'>
                                 <div className='card-body'>
                                     <h5 className='card-title text-center'>Hero Banner</h5>
                                     <p className='card-text text-center'><small className='text-muted'>Card 0</small></p>
                                 </div>
                             </div>
 
-                            <div className=' col-12 card m-2 shadow'>
+                            <div className='col-12 card p-3 m-2 shadow'>
                                 <div className='card-body'>
                                     <h5 className='card-title text-center'>Vantagens App</h5>
                                     <p className='card-text text-center'><small className='text-muted'>Card </small></p>
                                 </div>
                             </div>
 
-                            <div className='col-12 card m-2 shadow'>
+                            <div className='col-12 card p-3  m-2 shadow'>
                                 <div className='card-body '>
                                     <h5 className='card-title text-center'>Publicidade Agente Turístico</h5>
                                     <p className='card-text text-center'><small className='text-muted'>Card 2</small></p>
@@ -44,28 +44,35 @@ export default function GerirMicrosite() {
 
                 <div className='row'>
                     <div className='col-12'>
-                        <div className='row card-group'>
-                            <div className='col-12 card m-2 shadow'>
-                                <div className='card-body'>
-                                    <h5 className='card-title text-center'>Descarregar App</h5>
-                                    <p className='card-text text-center'><small className='text-muted'>Card 3</small></p>
+                        
+                        <div className='row'>
+                            <div className='Col-18'>
+
+                                <div className='row card-group'>
+                                    <div className='col-4 card p-3 m-2 shadow'>
+                                        <div className='card-body'>
+                                            <h5 className='card-title text-center'>Descarregar App</h5>
+                                            <p className='card-text text-center'><small className='text-muted'>Card 3</small></p>
+                                        </div>
+                                    </div>
+
+                                    <div className=' col-4 card m-2 shadow'>
+                                        <div className='card-body'>
+                                            <h5 className='card-title text-center'>Footer</h5>
+                                            <p className='card-text text-center'><small className='text-muted'>Rodapé</small></p>
+                                        </div>
+                                    </div>
+
+                                    <div className=' col-4 card m-2 shadow'>
+                                        <div className='card-body'>
+                                            <h5 className='card-title text-center'>+</h5>
+                                            <p className='card-text text-center'><small className='text-muted'>Adicionar Conteudo</small></p>
+                                        </div>
+                                    </div>
+
                                 </div>
                             </div>
-
-                            <div className=' col-12 card m-2 shadow'>
-                                <div className='card-body'>
-                                    <h5 className='card-title text-center'>Footer</h5>
-                                    <p className='card-text text-center'><small className='text-muted'>Rodapé</small></p>
-                                </div>
-                            </div>
-
-                            <div className=' col-12 card m-2 shadow'>
-                                <div className='card-body'>
-                                    <h5 className='card-title text-center'>+</h5>
-                                    <p className='card-text text-center'><small className='text-muted'>Adicionar Conteudo</small></p>
-                                </div>
-                            </div>
-
+            
                         </div>
                     </div>
                 </div>
