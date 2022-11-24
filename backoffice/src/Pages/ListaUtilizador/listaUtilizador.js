a29dca2aa76ca8ffd6db122c36d6e24a7ead2fbc
Mariana Castanheira <104756010+mansdmn@users.noreply.github.com>
2022-11-24 23:25:59 +0000

    texte das listas

--- backoffice/src/Pages/ListaUtilizador/listaUtilizador.js
+++ backoffice/src/Pages/ListaUtilizador/listaUtilizador.js
@@ -4,9 +4,68 @@ import React from 'react'
 export default function ListaUtilizador(props){
 
     return (
-       <div className='card'>
-        <div className='card-body'> ola eu sou toto</div>
+       <div className='row'>
+            <div className='col-12'>
+                <div className='card p-3 mb-5 shadow bg-body rouded'>
+                    <div className='row p-3'>
+                        <div className='col-12'>
+                            <div className='row'>
+                                
+                                
+                                <div className='row'>
+                                    <div className='col-12 pb-4'>
+                                        <div className='list-group list-group-flush'>
+                                            <div className='list-group-item '>
+                                                <div className='row'>
+                                                    <div className='col-5 pb-1'>
+                                                        <text>02 José António Gomes</text>
+                                                        <div className='col-1'>
+                                                            <button className=' btn btn-outline-danger bi bi-trash-fill me-md-2'></button> 
+                                                        </div> 
+                                                        <div className='col-1'>
+                                                            <button className='btn btn-outline-warning bi bi-pencil-fill'></button> 
+                                                         </div>  
+                                                    </div>
+                                                </div> 
+                                                                                    
+                                            </div>   
 
-       </div>        
+                                            <div className='list-group-item'>
+                                                <div className='row'>
+                                                    <div className='col-5 pb-1'>
+                                                        <text>02 José António Gomes</text>
+                                                    </div>
+                                                </div> 
+                                                <div className='col-1'>
+                                                    <button className=' btn btn-outline-danger bi bi-trash-fill me-md-2'></button> 
+                                                </div> 
+                                                <div className='col-1'>
+                                                    <button className='btn btn-outline-warning bi bi-pencil-fill'></button> 
+                                                </div>                                          
+                                            </div>                                    
+                                        </div>
+                                    </div>
+                                </div>
+
+                                    
+                                
+
+                                        <div className='col-10 pb-1'>
+                                            <input type='text' className='form-control' id='formGroupExampleInput' placeholder='Example input placeholder'/>                                
+                                         </div>
+                                        <div className='col-1'>
+                                            <button className=' btn btn-outline-danger bi bi-trash-fill me-md-2'></button> 
+                                        </div> 
+                                        <div className='col-1'>
+                                            <button className='btn btn-outline-warning bi bi-pencil-fill'></button> 
+                                        </div>   
+
+                                        
+                            </div>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+       </div>   
     );      
 }
