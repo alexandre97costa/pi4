a6ff45d65525351215d3b33d7cbff7698d138669
Mariana Castanheira <104756010+mansdmn@users.noreply.github.com>
2022-11-25 12:49:27 +0000

    ListaDeUtilizadores

--- backoffice/src/Pages/ListaUtilizador/listaUtilizador.js
+++ backoffice/src/Pages/ListaUtilizador/listaUtilizador.js
@@ -15,35 +15,203 @@ export default function ListaUtilizador(props){
                                 <div className='row'>
                                     <div className='col-12 pb-4'>
                                         <div className='list-group list-group-flush'>
+
+                                        <div className='list-group-item '>
+                                                <div className='row'>
+                                                    <div className='col-12 pb-1'>
+                                                        <div className='row'>
+                                                            <div className='col-5'>
+                                                                <p># Nome</p>
+                                                            </div> 
+                                                            <div className='col-5'>
+                                                                <p>Categoria</p>
+                                                            </div>  
+                                                            <div className='col-1'>
+                                                                <p>Ações</p>
+                                                            </div>                                                             
+                                                        </div>
+                                                    </div>
+                                                </div>                                
+                                            </div> 
+
+
                                             <div className='list-group-item '>
                                                 <div className='row'>
                                                     <div className='col-12 pb-1'>
-                                                        
-                                                        <text>02 José António Gomes</text>
-                                                        <div className='col-1'>
-                                                            <button className=' btn btn-outline-danger bi bi-trash-fill me-md-2'></button> 
-                                                        </div> 
-                                                        <div className='col-1'>
-                                                            <button className='btn btn-outline-warning bi bi-pencil-fill'></button> 
-                                                         </div>  
-                                                    </div>
-                                                </div> 
-                                                                                    
-                                            </div>   
+                                                        <div className='row'>
+                                                            <div className='col-5'>
+                                                                <p>02 José António Gomes</p>
+                                                            </div>
+                                                            <div class="d-inline p-2 bg-dark text-white text-center rounded-2 col-2 ">Responsável de Região</div>
+                                                            
+                                                            <div className='col-md-2 ms-auto'>
+                                                                <button className=' btn btn-outline-danger bi bi-trash-fill me-md-2'></button> 
+                                                                <button className='btn btn-outline-warning bi bi-pencil-fill'></button> 
+                                                            </div> 
 
-                                            <div className='list-group-item'>
-                                                <div className='row'>
-                                                    <div className='col-5 pb-1'>
-                                                        <text>02 José António Gomes</text>
+                                                        </div>
                                                     </div>
-                                                </div> 
-                                                <div className='col-1'>
-                                                    <button className=' btn btn-outline-danger bi bi-trash-fill me-md-2'></button> 
-                                                </div> 
-                                                <div className='col-1'>
-                                                    <button className='btn btn-outline-warning bi bi-pencil-fill'></button> 
-                                                </div>                                          
-                                            </div>                                    
+                                                </div>                                
+                                            </div> 
+
+                                            <div className='list-group-item '>
+                                                <div className='row'>
+                                                    <div className='col-12 pb-1'>
+                                                        <div className='row'>
+                                                            <div className='col-5'>
+                                                                <p>11 Luisa Machado Castro</p>
+                                                            </div>
+                                                            <div class="d-inline p-2 bg-dark text-white text-center rounded-2 col-2 ">Agente Turístico</div>
+                                                            
+                                                            <div className='col-md-2 ms-auto'>
+                                                                <button className=' btn btn-outline-danger bi bi-trash-fill me-md-2'></button> 
+                                                                <button className='btn btn-outline-warning bi bi-pencil-fill'></button> 
+                                                            </div> 
+
+                                                        </div>
+                                                    </div>
+                                                </div>                                
+                                            </div>
+
+                                            <div className='list-group-item '>
+                                                <div className='row'>
+                                                    <div className='col-12 pb-1'>
+                                                        <div className='row'>
+                                                            <div className='col-5'>
+                                                                <p>21 Rodrigo Miguel</p>
+                                                            </div>
+                                                            <div class="d-inline p-2 bg-dark text-white text-center rounded-2 col-2 ">Visitante</div>
+                                                            
+                                                            <div className='col-md-2 ms-auto'>
+                                                                <button className=' btn btn-outline-danger bi bi-trash-fill me-md-2'></button> 
+                                                                <button className='btn btn-outline-warning bi bi-pencil-fill'></button> 
+                                                            </div> 
+
+                                                        </div>
+                                                    </div>
+                                                </div>                                
+                                            </div> 
+
+                                            <div className='list-group-item '>
+                                                <div className='row'>
+                                                    <div className='col-12 pb-1'>
+                                                        <div className='row'>
+                                                            <div className='col-5'>
+                                                                <p>31 Amália Rodrigues Silva</p>
+                                                            </div>
+                                                            <div class="d-inline p-2 bg-dark text-white text-center rounded-2 col-2 ">Responsável de Região</div>
+                                                            
+                                                            <div className='col-md-2 ms-auto'>
+                                                                <button className=' btn btn-outline-danger bi bi-trash-fill me-md-2'></button> 
+                                                                <button className='btn btn-outline-warning bi bi-pencil-fill'></button> 
+                                                            </div> 
+
+                                                        </div>
+                                                    </div>
+                                                </div>                                
+                                            </div> 
+
+                                            <div className='list-group-item '>
+                                                <div className='row'>
+                                                    <div className='col-12 pb-1'>
+                                                        <div className='row'>
+                                                            <div className='col-5'>
+                                                                <p>36 Dinis Martins</p>
+                                                            </div>
+                                                            <div class="d-inline p-2 bg-dark text-white text-center rounded-2 col-2 ">Agente Turístico</div>
+                                                            
+                                                            <div className='col-md-2 ms-auto'>
+                                                                <button className=' btn btn-outline-danger bi bi-trash-fill me-md-2'></button> 
+                                                                <button className='btn btn-outline-warning bi bi-pencil-fill'></button> 
+                                                            </div> 
+
+                                                        </div>
+                                                    </div>
+                                                </div>                                
+                                            </div> 
+
+                                            <div className='list-group-item '>
+                                                <div className='row'>
+                                                    <div className='col-12 pb-1'>
+                                                        <div className='row'>
+                                                            <div className='col-5'>
+                                                                <p>22 Priscila Lopes</p>
+                                                            </div>
+                                                            <div class="d-inline p-2 bg-dark text-white text-center rounded-2 col-2 ">Agente Turístico</div>
+                                                            
+                                                            <div className='col-md-2 ms-auto'>
+                                                                <button className=' btn btn-outline-danger bi bi-trash-fill me-md-2'></button> 
+                                                                <button className='btn btn-outline-warning bi bi-pencil-fill'></button> 
+                                                            </div> 
+
+                                                        </div>
+                                                    </div>
+                                                </div>                                
+                                            </div> 
+
+                                            <div className='list-group-item '>
+                                                <div className='row'>
+                                                    <div className='col-12 pb-1'>
+                                                        <div className='row'>
+                                                            <div className='col-5'>
+                                                                <p>25 Mateus Almeida</p>
+                                                            </div>
+                                                            <div class="d-inline p-2 bg-dark text-white text-center rounded-2 col-2 ">Visitante</div>
+                                                            
+                                                            <div className='col-md-2 ms-auto'>
+                                                                <button className=' btn btn-outline-danger bi bi-trash-fill me-md-2'></button> 
+                                                                <button className='btn btn-outline-warning bi bi-pencil-fill'></button> 
+                                                            </div> 
+
+                                                        </div>
+                                                    </div>
+                                                </div>                                
+                                            </div> 
+
+                                            <div className='list-group-item '>
+                                                <div className='row'>
+                                                    <div className='col-12 pb-1'>
+                                                        <div className='row'>
+                                                            <div className='col-5'>
+                                                                <p>09 Sara Martins Campos</p>
+                                                            </div>
+                                                            <div class="d-inline p-2 bg-dark text-white text-center rounded-2 col-2 ">Responsável de Região</div>
+                                                            
+                                                            <div className='col-md-2 ms-auto'>
+                                                                <button className=' btn btn-outline-danger bi bi-trash-fill me-md-2'></button> 
+                                                                <button className='btn btn-outline-warning bi bi-pencil-fill'></button> 
+                                                            </div> 
+
+                                                        </div>
+                                                    </div>
+                                                </div>                                
+                                            </div> 
+
+                                            <div className='list-group-item '>
+                                                <div className='row'>
+                                                    <div className='col-12 pb-1'>
+                                                        <div className='row'>
+                                                            <div className='col-5'>
+                                                                <p>07 João Pedro Pereira</p>
+                                                            </div>
+                                                            <div class="d-inline p-2 bg-dark text-white text-center rounded-2 col-2 ">Administrador</div>
+                                                            
+                                                            <div className='col-md-2 ms-auto'>
+                                                                <button className=' btn btn-outline-danger bi bi-trash-fill me-md-2'></button> 
+                                                                <button className='btn btn-outline-warning bi bi-pencil-fill'></button> 
+                                                            </div> 
+
+                                                        </div>
+                                                    </div>
+                                                </div>                                
+                                            </div> 
+
+                                            <div className='list-group-item '>
+                                                <div className='row'>
+                                                    <div className='col-12 pb-1'>
+                                                    </div>
+                                                </div>                                
+                                            </div>  
                                         </div>
                                     </div>
                                 </div>
@@ -51,15 +219,7 @@ export default function ListaUtilizador(props){
                                     
                                 
 
-                                        <div className='col-10 pb-1'>
-                                            <input type='text' className='form-control' id='formGroupExampleInput' placeholder='Example input placeholder'/>                                
-                                         </div>
-                                        <div className='col-1'>
-                                            <button className=' btn btn-outline-danger bi bi-trash-fill me-md-2'></button> 
-                                        </div> 
-                                        <div className='col-1'>
-                                            <button className='btn btn-outline-warning bi bi-pencil-fill'></button> 
-                                        </div>   
+                                        
 
                                         
                             </div>
