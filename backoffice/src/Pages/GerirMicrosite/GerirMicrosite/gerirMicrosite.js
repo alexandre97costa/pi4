8c45e0c0bc8a35fc97be89d31ffda5ff7a3affd2
Raquelmeida <95857867+Raquelmeida@users.noreply.github.com>
2022-11-25 10:01:03 +0000

    Update gerirMicrosite.js

--- backoffice/src/Pages/GerirMicrosite/GerirMicrosite/gerirMicrosite.js
+++ backoffice/src/Pages/GerirMicrosite/GerirMicrosite/gerirMicrosite.js
@@ -37,45 +37,38 @@ export default function GerirMicrosite() {
                                 </div>
                             </div>
                         </div>
-
                     </div>
-
                 </div>
 
                 <div className='row'>
-                    <div className='col-12'>
-                        
-                        <div className='row'>
-                            <div className='Col-18'>
-
-                                <div className='row card-group'>
-                                    <div className='col-4 card p-3 m-2 shadow'>
-                                        <div className='card-body'>
-                                            <h5 className='card-title text-center'>Descarregar App</h5>
-                                            <p className='card-text text-center'><small className='text-muted'>Card 3</small></p>
-                                        </div>
-                                    </div>
-
-                                    <div className=' col-4 card m-2 shadow'>
-                                        <div className='card-body'>
-                                            <h5 className='card-title text-center'>Footer</h5>
-                                            <p className='card-text text-center'><small className='text-muted'>Rodapé</small></p>
-                                        </div>
-                                    </div>
-
-                                    <div className=' col-4 card m-2 shadow'>
-                                        <div className='card-body'>
-                                            <h5 className='card-title text-center'>+</h5>
-                                            <p className='card-text text-center'><small className='text-muted'>Adicionar Conteudo</small></p>
-                                        </div>
-                                    </div>
-
+                    <div className='col-10'>
+                        <div className='row card-group'>
+                            <div className='col-4 card  p-3 m-2 shadow'>
+                                <div className='card-body'>
+                                    <h5 className='card-title text-center'>Descarregar App</h5>
+                                    <p className='card-text text-center'><small className='text-muted'>Card 3</small></p>
                                 </div>
                             </div>
-            
+
+                            <div className=' col-4 card p-3  m-2 shadow'>
+                                <div className='card-body'>
+                                    <h5 className='card-title text-center'>Footer</h5>
+                                    <p className='card-text text-center'><small className='text-muted'>Rodapé</small></p>
+                                </div>
+                            </div>
+
+                            <div className='col-3 card p-3 m-2 shadow'>
+                                <div className='card-body'>
+                                    <h5 className='card-title text-center'>+</h5>
+                                    <p className='card-text text-center'><small className='text-muted'> Adicionar Conteúdo </small></p>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
+
+            
             </div>
         </div>
     );
