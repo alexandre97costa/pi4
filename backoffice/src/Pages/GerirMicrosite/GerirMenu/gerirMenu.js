59891f24aa8b81129ffabf3e1379cdb4ffe13a81
Raquelmeida <95857867+Raquelmeida@users.noreply.github.com>
2022-11-25 10:25:31 +0000

    adicionar container

--- backoffice/src/Pages/GerirMicrosite/GerirMenu/gerirMenu.js
+++ backoffice/src/Pages/GerirMicrosite/GerirMenu/gerirMenu.js
@@ -1,27 +1,29 @@
 import React from 'react';
 
-export default function GerirMenu(){
-    return(
-        <div className='row'>
-            <div className='col-12'>
-                <p className='fs-3 mb-5'>Gerir Menu</p>
+export default function GerirMenu() {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col-12'>
+                    <p className='fs-3 mb-5'>Gerir Menu</p>
 
-                <div className='card p-3 mb-5 shadow bg-body rounded'>
-                    <div className='row p-3'>
-                        <div className='col-12'>
-                            <div className='row'>
-                                <div className="col-12">
-                                    <label htmlFor='formGroupExampleInput' className='form-label'>Inserir Logo</label>
-                                </div>
+                    <div className='card p-3 mb-5 shadow bg-body rounded'>
+                        <div className='row p-3'>
+                            <div className='col-12'>
                                 <div className='row'>
-                                    <div className='col-9 pb-4'>
-                                        <input type='text' className='form-control' id='formGroupExampleInput' placeholder='Example input placeholder'/>
+                                    <div className="col-12">
+                                        <label htmlFor='formGroupExampleInput' className='form-label'>Inserir Logo</label>
                                     </div>
-                                    <div className='col-1'>
-                                    <button className=' btn btn-outline-danger bi bi-trash-fill me-md-2'></button>
-                                    </div>
-                                    <div className='col-1'>
-                                    <button className='btn btn-outline-warning bi bi-pencil-fill'></button>
+                                    <div className='row'>
+                                        <div className='col-9 pb-4'>
+                                            <input type='text' className='form-control' id='formGroupExampleInput' placeholder='Example input placeholder' />
+                                        </div>
+                                        <div className='col-1'>
+                                            <button className=' btn btn-outline-danger bi bi-trash-fill me-md-2'></button>
+                                        </div>
+                                        <div className='col-1'>
+                                            <button className='btn btn-outline-warning bi bi-pencil-fill'></button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -29,8 +31,10 @@ export default function GerirMenu(){
                     </div>
                 </div>
             </div>
+
         </div>
+
     );
 
-    
+
 }
--- backoffice/src/Pages/GerirMicrosite/GerirMicrosite/gerirMicrosite.js
+++ backoffice/src/Pages/GerirMicrosite/GerirMicrosite/gerirMicrosite.js
@@ -2,73 +2,73 @@ import React from 'react';
 
 export default function GerirMicrosite() {
     return (
-        <div className='row gx-3'>
-            <div className='col-12'>
-                <p className='fs-3 mb-5'> Gerir Microsite</p>
+        <div className='container-fluid'>
+            <div className='row gx-3'>
+                <div className='col-12'>
+                    <p className='fs-3 mb-5'> Gerir Microsite</p>
 
-                <div className='row'>
-                    <div className='col-12'>
-                        <div className='row card-group'>
-                            <div className='col-12 card  p-3 m-2 shadow'>
-                                <div className='card-body'>
-                                    <h5 className='card-title text-center'>Menu</h5>
-                                    <p className='card-text text-center'><small className='text-muted'>Cabeçalho</small></p>
+                    <div className='row'>
+                        <div className='col-12'>
+                            <div className='row card-group text'>
+                                <div className='col-12 card  p-3 m-2 shadow rounded-3'>
+                                    <div className='card-body'>
+                                        <h5 className='card-title text-center'>Menu</h5>
+                                        <p className='card-text text-center'><small className='text-muted'>Cabeçalho</small></p>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div className=' col-12 card p-3  m-2 shadow'>
-                                <div className='card-body'>
-                                    <h5 className='card-title text-center'>Hero Banner</h5>
-                                    <p className='card-text text-center'><small className='text-muted'>Card 0</small></p>
+                                <div className=' col-12 card p-3  m-2 shadow rounded-3'>
+                                    <div className='card-body'>
+                                        <h5 className='card-title text-center'>Hero Banner</h5>
+                                        <p className='card-text text-center'><small className='text-muted'>Card 0</small></p>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div className='col-12 card p-3 m-2 shadow'>
-                                <div className='card-body'>
-                                    <h5 className='card-title text-center'>Vantagens App</h5>
-                                    <p className='card-text text-center'><small className='text-muted'>Card </small></p>
+                                <div className='col-12 card p-3 m-2 shadow rounded-3'>
+                                    <div className='card-body'>
+                                        <h5 className='card-title text-center'>Vantagens App</h5>
+                                        <p className='card-text text-center'><small className='text-muted'>Card </small></p>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div className='col-12 card p-3  m-2 shadow'>
-                                <div className='card-body '>
-                                    <h5 className='card-title text-center'>Publicidade Agente Turístico</h5>
-                                    <p className='card-text text-center'><small className='text-muted'>Card 2</small></p>
+                                <div className='col-12 card p-3  m-2 shadow rounded-3'>
+                                    <div className='card-body '>
+                                        <h5 className='card-title text-center'>Publicidade Agente Turístico</h5>
+                                        <p className='card-text text-center'><small className='text-muted'>Card 2</small></p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                <div className='row'>
-                    <div className='col-10'>
-                        <div className='row card-group'>
-                            <div className='col-4 card  p-3 m-2 shadow'>
-                                <div className='card-body'>
-                                    <h5 className='card-title text-center'>Descarregar App</h5>
-                                    <p className='card-text text-center'><small className='text-muted'>Card 3</small></p>
+                    <div className='row'>
+                        <div className='col-10'>
+                            <div className='row card-group'>
+                                <div className='col-4 card  p-3 m-2 shadow rounded-3'>
+                                    <div className='card-body'>
+                                        <h5 className='card-title text-center'>Descarregar App</h5>
+                                        <p className='card-text text-center'><small className='text-muted'>Card 3</small></p>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div className=' col-4 card p-3  m-2 shadow'>
-                                <div className='card-body'>
-                                    <h5 className='card-title text-center'>Footer</h5>
-                                    <p className='card-text text-center'><small className='text-muted'>Rodapé</small></p>
+                                <div className=' col-4 card p-3  m-2 shadow rounded-3'>
+                                    <div className='card-body'>
+                                        <h5 className='card-title text-center'>Footer</h5>
+                                        <p className='card-text text-center'><small className='text-muted'>Rodapé</small></p>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div className='col-3 card p-3 m-2 shadow'>
-                                <div className='card-body'>
-                                    <h5 className='card-title text-center'>+</h5>
-                                    <p className='card-text text-center'><small className='text-muted'> Adicionar Conteúdo </small></p>
+                                <div className='col-3 card p-3 m-2 shadow rounded-3'>
+                                    <div className='card-body'>
+                                        <h5 className='card-title text-center'>+</h5>
+                                        <p className='card-text text-center'><small className='text-muted'> Adicionar Conteúdo </small></p>
+                                    </div>
                                 </div>
-                            </div>
 
+                            </div>
                         </div>
                     </div>
                 </div>
-
-            
             </div>
         </div>
     );
