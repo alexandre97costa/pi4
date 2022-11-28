e8176b52c6ac428707426aadb9fb2f7bca2c7670
Raquelmeida <95857867+Raquelmeida@users.noreply.github.com>
2022-11-28 16:27:11 +0000

    Update atRecompensas.js

--- backoffice/src/Pages/ATRecompensas/atRecompensas.js
+++ backoffice/src/Pages/ATRecompensas/atRecompensas.js
@@ -1,12 +1,21 @@
 import React from "react";
 
-export function ATRecompensas(props){
-    return(
-        <div className='container-fluid'>
-            <div className='w-50 bg-primary me-2 d-inline-block'>234
-
-            </div>
+export function ATRecompensas(props) {
+  return (
+    <div className="container-fluid">
+      <div class="card">
+        <h5 class="card-header">Featured</h5>
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+          <a href="#" class="btn btn-primary">
+            Go somewhere
+          </a>
         </div>
-
-    );
+      </div>
+    </div>
+  );
 }
