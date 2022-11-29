0a4b58449d3ced96bbfe12ae6b7c1c25ec1c4560
Raquelmeida <95857867+Raquelmeida@users.noreply.github.com>
2022-11-29 10:11:35 +0000

    update componente do card do microste

--- backoffice/src/Components/CardsMicrosite.js
+++ backoffice/src/Components/CardsMicrosite.js
@@ -2,27 +2,18 @@ import React from "react";
 
 export default function CardsMicrosite(props) {
   return (
-    <div className="row pb-4 justify-content-center">
-      <div className="col-12 ">
-
-        <div className="row">
-          <div className="col-12">
-
-            <div className="row card-group text">
-
-              <div className="col-12 card  p-3 m-2 shadow rounded-3">
-                <div className="card-body">
-                  <h5 className="card-title text-center">{props.titulo}</h5>
-                  <p className="card-text text-center">
-                    <small className="text-muted">{props.subTitulo}</small>
-                  </p>
-                </div>
-              </div>
-              
-            </div>
-          </div>
+    <div className="container-fluid">
+      <div className="row">
+      <div className="col-12">
+        <div className="card-body rounded-3 shadow p-5 m-4 ">
+          <h5 className="card-title text-center">{props.titulo}</h5>
+          <p className="card-text text-center">
+            <small className="text-muted">{props.subTitulo}</small>
+          </p>
         </div>
       </div>
     </div>
+    </div>
+    
   );
 }
--- backoffice/src/Pages/GerirMicrosite/GerirMicrosite/gerirMicrosite.js
+++ backoffice/src/Pages/GerirMicrosite/GerirMicrosite/gerirMicrosite.js
@@ -4,20 +4,28 @@ import CardsMicrosite from "../../../Components/CardsMicrosite";
 export default function GerirMicrosite() {
   return (
     <div className="container-fluid">
-      <div className="row gx-3">
+      <div className="row">
         <div className="col-12">
           <p className="fs-3 mb-5"> Gerir Microsite</p>
 
-          <div className="card p-3 mb-5 ms-4 shadow bg-body rounded">
+          <div className="card shadow bg-body rounded">
             <div className="row">
               <div className="col-12">
-                <CardsMicrosite titulo="Menu" subTitulo="cabeçalho" />
-                <CardsMicrosite titulo="Menu" subTitulo="cabeçalho" />
-                <CardsMicrosite titulo="Menu" subTitulo="cabeçalho" />
-                <CardsMicrosite titulo="Menu" subTitulo="cabeçalho" />
+                <div className=" row row-cols-4">
+                  <CardsMicrosite titulo="Menu" subTitulo="cabeçalho" />
+                  <CardsMicrosite titulo="Menu" subTitulo="cabeçalho" />
+                  <CardsMicrosite titulo="Menu" subTitulo="cabeçalho" />
+                  <CardsMicrosite titulo="Menu" subTitulo="cabeçalho" />
+                </div>
               </div>
             </div>
-          </div>
+                <div className=" row row-cols-12">
+                  <CardsMicrosite titulo="Menu" subTitulo="cabeçalho" />
+                  <CardsMicrosite titulo="Menu" subTitulo="cabeçalho" />
+                  <CardsMicrosite titulo="Menu" subTitulo="cabeçalho" />
+                </div>
+              </div>
+
         </div>
       </div>
     </div>
