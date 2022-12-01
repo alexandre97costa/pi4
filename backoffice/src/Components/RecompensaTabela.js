cf78544af2ac68ac309175656704025c88b8845e
Raquelmeida <95857867+Raquelmeida@users.noreply.github.com>
2022-12-01 00:05:53 +0000

    update quarta componentes recompensas

--- backoffice/src/Components/RecompensaTabela.js
+++ backoffice/src/Components/RecompensaTabela.js
@@ -10,7 +10,17 @@ export function RecompensaTabela(props) {
               <div li className="list-group-item d-flex justify-content-between align-items-center"> {props.nomePI}
                 <button className= "btn btn-Light "> + Criar recompensa</button>
               </div>
-              <div li className="list-group-item d-flex justify-content-between align-items-center">{props.nomeRecompensa0}</div>
+              <div li className="list-group-item d-flex justify-content-between align-items-center">
+                <div className="row">
+                  <div className="col-6">
+                    {props.nomeRecompensa0}
+                  </div>
+                </div>
+                <div className="col-5">
+                  <button className="btn btn-outline-warning bi bi-pencil-fill me-md-3">Editar</button>
+                  <button className=" btn btn-outline-danger bi bi-trash-fill">Eliminar</button>
+                </div>
+              </div>
               <div li className="list-group-item d-flex justify-content-between align-items-center">{props.nomeRecompensa1}</div>
               <div li className="list-group-item d-flex justify-content-between align-items-center">{props.nomeRecompensa2}</div>
               <div li className="list-group-item d-flex justify-content-between align-items-center">{props.nomeRecompensa3}</div>
--- backoffice/src/Pages/ATRecompensas/atRecompensas.js
+++ backoffice/src/Pages/ATRecompensas/atRecompensas.js
@@ -3,16 +3,26 @@ import { RecompensaTabela } from "../../Components/RecompensaTabela";
 
 export function ATRecompensas(props) {
   return (
-    <div className="container-fluid">
+    <div className="container">
       <div className="row">
         <div className="col-6">
           <RecompensaTabela
           nomePI="Palácio do Gelo"
-          nomeRecompensa0="1 Gelado no AMOR MEU"
-          nomeRecompensa2="1 Gelado no AMOR MEU"
-          nomeRecompensa3="1 Gelado no AMOR MEU"
-          nomeRecompensa4="1 Gelado no AMOR MEU"
-          nomeRecompensa1="1 Gelado no AMOR NOSSO"
+          nomeRecompensa0="Recompensa A"
+          nomeRecompensa2="Recompensa B"
+          nomeRecompensa3="Recompensa C"
+          nomeRecompensa4="Recompensa D"
+          nomeRecompensa1="Recompensa E"
+          />
+        </div>
+        <div className="col-6">
+          <RecompensaTabela
+          nomePI="Palácio do Gelo"
+          nomeRecompensa0="Recompensa A"
+          nomeRecompensa2="Recompensa B"
+          nomeRecompensa3="Recompensa C"
+          nomeRecompensa4="Recompensa D"
+          nomeRecompensa1="Recompensa E"
           />
         </div>
       </div>
