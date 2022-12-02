06ebcdcd51bfae805ed2250d90af07d9093c5ddb
Raquelmeida <95857867+Raquelmeida@users.noreply.github.com>
2022-12-02 10:49:43 +0000

    Update gerirAdicionarCards.js
    adicionar conteudo - finalizado

--- backoffice/src/Pages/GerirMicrosite/GerirAdicionarCards/gerirAdicionarCards.js
+++ backoffice/src/Pages/GerirMicrosite/GerirAdicionarCards/gerirAdicionarCards.js
@@ -2,6 +2,7 @@ import React from "react";
 import TituloMicrosite from "../../../Components/TituloMicrosite";
 import SubTituloMicrosite from "../../../Components/SubTituloMicrosite";
 import UrlImagemMicrosite from "../../../Components/UrlImagemMicrosite";
+import BotaoMicrosite from "../../../Components/BotaoMicrosite";
 
 export default function GerirAdicionarCards() {
   return (
@@ -16,7 +17,11 @@ export default function GerirAdicionarCards() {
             <SubTituloMicrosite 
             subTitulo="Subtítulo"/>
             <UrlImagemMicrosite
-            urlImagemMicrosite="URL da imagem para o conteúdo"
+            urlImagem="URL da imagem"
+            urlImagemPlaceholder="Inserir URL"
+            />
+            <BotaoMicrosite
+            botao=" Texto Botão"
             />
           </div>
         </div>
