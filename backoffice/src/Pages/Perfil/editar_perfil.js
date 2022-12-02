556d7bf0f441815b80ef6bb083cd154c34835714
Tiago Almeida <103502300+Tigas14@users.noreply.github.com>
2022-12-02 12:05:58 +0000

    Editar Perfil

---
+++ backoffice/src/Pages/Perfil/editar_perfil.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Sidebar from "../../Components/sidebar";
+
+export default function GerirAdicionarCards() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-10">
+          <p className="fs-3 mb-5 ms-4">Editar Perfil</p>
+
+          <div className="card p-3 mb-5 ms-4 shadow bg-body rounded">
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- backoffice/src/Pages/index.js
+++ backoffice/src/Pages/index.js
@@ -1,7 +1,8 @@
 import Teste from "./Teste/teste"
 import Login from "./Login/login"
 import Recuperar from "./Login/recuperar"
+import EditarPerfil from "./Perfil/editar_perfil"
 import PontoInteresse from './PontosInteresse/ponto_interesse'
 
-const Pages = { Teste, Login, PontoInteresse, Recuperar }
+const Pages = { Teste, Login, PontoInteresse, Recuperar, EditarPerfil }
 export default Pages
