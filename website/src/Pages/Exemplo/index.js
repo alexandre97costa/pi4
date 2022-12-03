668b1a45d84a4e41674111d4f4a0a8322202fcc0
Raquelmeida <95857867+Raquelmeida@users.noreply.github.com>
2022-12-03 22:14:24 +0000

    update website
    criacao css, import no main

--- website/src/Pages/Exemplo/index.js
+++ website/src/Pages/Exemplo/index.js
@@ -1,28 +1,26 @@
 export function Exemplo() {
   return (
-    <div className="row">
-      <div className="col-12">
-        <div className="row">
-          <div className="col-12">
-            <img src="..." class="float-start" alt="..."></img>
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <div className="row">
+            <div className="col-12">
+              <img src="..." class="float-start" alt="..."></img>
+            </div>
           </div>
-        </div>
 
-        <div className="row">
-          <div className="col-12">
-            <div className="card text-center">
-              <div className="card-header">Featured</div>
-              <div className="card-body">
-                <h5 className="card-title">Special title treatment</h5>
-                <p className="card-text">
-                  With supporting text below as a natural lead-in to additional
-                  content.
-                </p>
-                <a href="#" class="btn btn-primary">
-                  Go somewhere
-                </a>
+          <div className="row">
+            <div className="col-12">
+              <div className="card bg-secondary text-center">
+                <div className="card-tittle text-start">dasdasdasd</div>
+                <div className="row me-4 mt-2 mb-2">
+                  <div className="col-2">
+                    <button type="button" className="btn btn-primary ms-3">
+                      dfhdfghdfgh
+                    </button>
+                  </div>
+                </div>
               </div>
-              <div className="card-footer text-muted">2 days ago</div>
             </div>
           </div>
         </div>
