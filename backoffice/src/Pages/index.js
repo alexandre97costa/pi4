d803f6d8acfac622011358eb4c93910a0caae129
Joao Sequeira <joaosequeira28@hotmail.com>
2022-12-09 11:27:11 +0000

    uupdate

--- backoffice/src/Pages/index.js
+++ backoffice/src/Pages/index.js
@@ -1,13 +1,9 @@
 import Teste from "./Teste/teste"
-import Login from "./Login/login"
 import ListaUtilizador from "./ListaUtilizador/listaUtilizador"
 import ListaVisitantes from "./ListaVisitantes/listaVisitantes"
 import ListaAgenteTuristico from "./ListaAgenteTuristico/listaAgente"
 import ListaResponsavelRegiao from "./ListaResponsavelRegiao/listaResponsavel"
 import ListaRegiao from "./ListaRegiao/listaRegiao"
-import Recuperar from "./Login/recuperar"
-import EditarPerfil from "./Perfil/editar_perfil"
-import EditarPass from "./Perfil/editar_pass"
 import GerirMicrosite from "./GerirMicrosite/GerirMicrosite/gerirMicrosite"
 import GerirVantagensApp from "./GerirMicrosite/GerirVantagensApp/gerirVantagensApp"
 import GerirDescarregarApp from "./GerirMicrosite/GerirDescarregarApp/gerirDescarregarApp"
@@ -17,8 +13,14 @@ import GerirHeroBanner from "./GerirMicrosite/GerirHeroBanner/gerirHeroBanner"
 import GerirPublicidadeAgenteTuristico from "./GerirMicrosite/GerirPublicidadeAgenteTuristico/gerirPublicidadeAgenteTuristico"
 import GerirAdicionarCards from "./GerirMicrosite/GerirAdicionarCards/gerirAdicionarCards"
 import ATRecompensas from "./ATRecompensas/atRecompensas"
-import InicioAT from "./Agentes_Turisticos/Inicio_AT"
 
-const Pages = { Teste, Login, GerirMicrosite, GerirVantagensApp, GerirDescarregarApp, GerirEditarFooter, GerirMenu, GerirHeroBanner, GerirPublicidadeAgenteTuristico,GerirAdicionarCards, ATRecompensas, ListaUtilizador, ListaVisitantes,ListaAgenteTuristico, ListaResponsavelRegiao, ListaRegiao, Recuperar, EditarPass, EditarPerfil, InicioAT}
+import EditarPasse from './Perfil/editarPasse'
+import EditarPerfil from './Perfil/editarPerfil'
+import Login from "./Login/login"
+import RecuperarPasse from './Login/recuperarPasse'
+
+
+
+const Pages = { Teste, Login, GerirMicrosite, GerirVantagensApp, GerirDescarregarApp, GerirEditarFooter, GerirMenu, GerirHeroBanner, GerirPublicidadeAgenteTuristico,GerirAdicionarCards, ATRecompensas, ListaUtilizador, ListaVisitantes,ListaAgenteTuristico, ListaResponsavelRegiao, ListaRegiao, RecuperarPasse, EditarPasse, EditarPerfil}
 
 export default Pages
