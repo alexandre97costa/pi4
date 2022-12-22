cd37f5d316ef25584d47684be7b37ec346eab9f5
Joao Sequeira <joaosequeira28@hotmail.com>
2022-12-22 18:08:09 +0000

    admin dashboard

--- backoffice/src/Pages/Admin/Home/home.js
+++ backoffice/src/Pages/Admin/Home/home.js
@@ -33,15 +33,15 @@ export default function Home() {
                     <p className="fs-5 text-body fw-light">Lista de Regiões</p>
                 </div>
 
-                <div className='col-12 col-md-2 text-center'>
-                    <Link to="/utilizadores" className='btn btn-agentes h-100 w-100 rounded-4 btn-primary fw-bold py-5'>Agentes Turísticos</Link>
+                <div className='col-12 mb-3 col-sm-4 mb-sm-0 text-break col-md-2 text-center'>
+                    <Link to="/utilizadores" className='btn btn-agentes h-100 w-100 rounded-4 text-white fw-bold py-5'>Agentes Turísticos</Link>
                 </div>
 
-                <div className='col-12 col-md-2 text-center'>
-                    <Link to="/utilizadores" className='btn btn-regiao h-100 w-100 rounded-4 btn-primary fw-bold py-5'>Responsáveis de Região</Link>
+                <div className='col-12 mb-3 col-sm-4 mb-sm-0 text-break col-md-2 text-center'>
+                    <Link to="/utilizadores" className='btn btn-regiao h-100 w-100 rounded-4 text-white fw-bold py-5'>Responsáveis de Região</Link>
                 </div>
 
-                <div className='col-12 col-md-2 text-center'>
+                <div className='col-12 mb-3 col-sm-4 mb-sm-0 text-break col-md-2 text-center'>
                     <Link to="/utilizadores" className='btn btn-primary h-100 w-100 rounded-4 fw-bold py-5'>Visitantes</Link>
                 </div>
 
