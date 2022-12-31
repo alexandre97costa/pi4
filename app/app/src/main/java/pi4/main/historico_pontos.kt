fd614995330906267f55eaf258227e60589794c7
Alexandre Costa <alexandre97costa@gmail.com>
2022-12-31 17:08:26 +0000

    dois ecrãs

---
+++ app/app/src/main/java/pi4/main/historico_pontos.kt
@@ -0,0 +1,11 @@
+package pi4.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class historico_pontos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_historico_pontos)
+    }
+}
