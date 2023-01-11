765923ad90066a2e1af2a65ef8e2290d21128357
Joao Sequeira <joaosequeira28@hotmail.com>
2023-01-11 12:17:42 +0000

    teste

---
+++ app/app/src/main/java/pi4/main/Activitys/Historico/ActivityInfoHistoricoEvento.kt
@@ -0,0 +1,12 @@
+package pi4.main.Activitys.Historico
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pi4.main.R
+
+class ActivityInfoHistoricoEvento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_historico_evento)
+    }
+}
