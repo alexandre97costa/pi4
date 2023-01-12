3186900b3f8200ea7c4ae8afdd69d8247872cdd5
Raquelmeida <95857867+Raquelmeida@users.noreply.github.com>
2023-01-12 15:44:28 +0000

    update resize cards microsite

--- backoffice/src/Components/Microsite/CardMicrosite.js
+++ backoffice/src/Components/Microsite/CardMicrosite.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 export default function CardMicrosite(props) {
   return (
-    <Link to={props.path} className="card text-decoration-none text-dark text-center shadow bg-body rounded h-100 cursor-pointer border-0" onClick={() => console.log(props.titulo)}>
-      <div className="card-body p-5 m-4">
+    <Link to={props.path} className="card text-decoration-none text-dark text-center  shadow bg-body rounded-4 h-100 cursor-pointer border-0" onClick={() => console.log(props.titulo)}>
+      <div className="card-body p-5 m-4 ">
         <h5 className={changeColorTitulo()}>{props.titulo}</h5>
         <p className="card-text text-center">
-          <small className="text-muted">{props.subTitulo}</small>
+          <small className="text-muted fw-semibold ">{props.subTitulo}</small>
         </p>
       </div>
     </Link>
@@ -15,7 +15,7 @@ export default function CardMicrosite(props) {
 
   function changeColorTitulo() {
     if (!props.color)
-      return "card-title fs-5 text-center"
-    return "card-title fs-5 text-center " + props.color;
+      return "card-title fs-5 text-center "
+    return "card-title fs-5 text-center fw-bolder " + props.color;
   }
 }
