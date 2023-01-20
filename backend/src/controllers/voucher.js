2ec35ea45e5b514626eb6e8f99f5e8c48f0eced0
Alexandre Costa <alexandre97costa@gmail.com>
2023-01-20 23:02:13 +0000

    cleanup

--- backend/src/controllers/voucher.js
+++ backend/src/controllers/voucher.js
@@ -6,27 +6,13 @@ const dev = new devClass;
 // * Como usar o Op:
 // * https://sequelize.org/docs/v6/core-concepts/model-querying-basics/#operators
 const {
-    candidatura_at,
-    comentario_avaliacao,
-    distrito,
-    evento,
-    freguesia,
-    imagem,
-    municipio,
     ponto_interesse,
     ponto_interesse_recompensa,
-    pontos_evento,
-    pontos_ponto_interesse,
     recompensa,
-    reserva,
-    sessao,
-    tipo_evento,
     tipo_interesse,
-    tipo_utilizador,
     utilizador,
     voucher
 } = sequelize.models
-// * no final, podem-se apagar os que não estão a ser usados
 
 
 module.exports = {
--- backend/src/routes/voucher.js
+++ backend/src/routes/voucher.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const v = require('../controllers/voucher')
 
 router.get('/', v.get)
 router.get('/:id', v.get)
+router.post('/', v.post)
 
-module.exports = router;
+module.exports = router
