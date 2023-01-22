856b338e292d1ffc7de2d4e68f16376afe4a2eaf
Joao Sequeira <joaosequeira28@hotmail.com>
2023-01-22 16:54:21 +0000

    data

--- app/app/src/main/java/pi4/main/Classes/Eventos.kt
+++ app/app/src/main/java/pi4/main/Classes/Eventos.kt
@@ -1,10 +1,9 @@
 package pi4.main.Classes
 
-class Eventos(id: String, nome: String, data: String ,descricao: String ,numPontos: Int, numVagas: Int, tipoEvento: String, listaSessoes: ArrayList<Sessao> ,pontoInteresseId: String) {
+class Eventos(id: String, nome: String ,descricao: String ,numPontos: Int, numVagas: Int, tipoEvento: String, listaSessoes: ArrayList<Sessao> ,pontoInteresseId: String) {
     val id: String
     val nome: String
     val descricao: String
-    val data: String
     val numPontos: Int
     val numVagas: Int
     val tipoEvento: String
@@ -15,7 +14,6 @@ class Eventos(id: String, nome: String, data: String ,descricao: String ,numPont
         this.id = id
         this.nome = nome
         this.descricao = descricao
-        this.data = data
         this.numPontos = numPontos
         this.numVagas = numVagas
         this.tipoEvento = tipoEvento
