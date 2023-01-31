bece051c373156520ec7c237296aa4d56e6d06e9
Mariana Castanheira <104756010+mansdmn@users.noreply.github.com>
2023-01-31 15:27:38 +0000

    ja entrei

--- backend/src/config/Database.js
+++ backend/src/config/Database.js
@@ -4,7 +4,7 @@ const { applyExtraSetup } = require('./extra-setup.js');
 const sequelize = new Sequelize(
     'pi4',              // nome da db
     'postgres',         // user
-    'ola',         // pass
+    '20447',         // pass
     {
         host: 'localhost',
         port: '5432',
