a60b8a462cab44565c71a865bdd311e75305c23b
Alexandre Costa <alexandre97costa@gmail.com>
2023-02-01 19:43:04 +0000

    voucher e reserva check

--- backend/src/config/Database.js
+++ backend/src/config/Database.js
@@ -4,7 +4,7 @@ const { applyExtraSetup } = require('./extra-setup.js');
 const sequelize = new Sequelize(
     'pi4',              // nome da db
     'postgres',         // user
-    '123',         // pass
+    'postgres',         // pass
     {
         host: 'localhost',
         port: '5432',
