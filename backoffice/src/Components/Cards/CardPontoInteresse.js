ab8a83597a4b57d52e126f2ad49ff0313e4fe352
Alex Ferreira <estgv16553@alunos.estgv.ipv.pt>
2023-02-05 18:00:39 +0000

    Régua e esquadro

--- backoffice/src/Components/Cards/CardPontoInteresse.js
+++ backoffice/src/Components/Cards/CardPontoInteresse.js
@@ -16,25 +16,25 @@ export default function CardPontoInteresse(props) {
   }
 
   return (
-    <div className="card border-0 rounded-4 shadow">
+    <div className="card border-0 rounded-4 shadow"  >
       <img src={props.imagem} className="card-img-top" />
 
-      <div className="card-body p-3">
+      <div className="card-body p-3 d-flex flex-column">
         <h5 className="card-title fs-4">{props.nome}</h5>
 
         <h6 className="card-subtitle fs-4 fw-light">{props.categoria}</h6>
 
-        <p className="card-text fs-5 fw-light mb-4">{props.morada}</p>
+        <p className="card-text fs-6 fw-light mb-4">{props.morada}</p>
 
-        <div className="fs-4 text-center mb-4">
-          <i className="bi bi-qr-code-scan text-success px-2">{props.numeroScans}</i>
-          {/* <i className="bi bi-chat text-primary px-2">{props.numeroComentarios}</i> */}
-          <i className="bi bi-star text-warning px-2">{props.numeroFavoritos}</i>
-          {/* <i className="bi bi-journal-check text-info px-2">{props.numeroCheck}</i> */}
+        <div className="fs-4 mb-4 mt-auto">
+          <div className="text-center">
+            <i className="bi bi-qr-code-scan text-success px-2">{props.numeroScans}</i>
+            {/* <i className="bi bi-chat text-primary px-2">{props.numeroComentarios}</i> */}
+            <i className="bi bi-star text-warning px-2">{props.numeroFavoritos}</i>
+            {/* <i className="bi bi-journal-check text-info px-2">{props.numeroCheck}</i> */}
+          </div>
+          {chooseTipoCartao(props.tipo)}
         </div>
-
-        {chooseTipoCartao(props.tipo)}
-
       </div>
     </div>
   );
