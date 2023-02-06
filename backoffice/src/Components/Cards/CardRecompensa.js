8f81d17b71e5244faa6cfcff97345f36d6b23299
Joao Sequeira <joaosequeira28@hotmail.com>
2023-02-06 23:31:56 +0000

    Update CardRecompensa.js

--- backoffice/src/Components/Cards/CardRecompensa.js
+++ backoffice/src/Components/Cards/CardRecompensa.js
@@ -7,7 +7,7 @@ export default function CardRecompensa(props) {
         <h5 className="card-title fs-4">
           <div className="row">
             <div className="col-8 col-md-9 align-self-center">{props.title}</div>
-            <div className="col-4 col-md-3 text-end"><button type="button" className="btn bi bi-trash3 text-danger" aria-label="Close" /></div>
+            {/* <div className="col-4 col-md-3 text-end"><button type="button" className="btn bi bi-trash3 text-danger" aria-label="Close" /></div> */}
           </div>
         </h5>
         <h6 className="card-title fw-normal">{props.categoria}</h6>
