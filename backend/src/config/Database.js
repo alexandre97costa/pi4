59c13329462ff0349e1cd468e043371bef61b7a7
Joao Sequeira <joaosequeira28@hotmail.com>
2023-02-06 19:33:45 +0000

    Update Database.js

--- backend/src/config/Database.js
+++ backend/src/config/Database.js
@@ -4,7 +4,7 @@ const { applyExtraSetup } = require('./extra-setup.js');
 const sequelize = new Sequelize(
     'pi4',              // nome da db
     'postgres',         // user
-    'olá amigos, isto é uma passe mesmo bueda grande para demorar mesmo totil tempo a mudarem a passe, topam? 😎',         // pass
+    'R7220810',         // pass
     {
         host: 'localhost',
         port: '5432',
