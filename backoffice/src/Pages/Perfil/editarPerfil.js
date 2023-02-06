909476717d4ab188c42a202806f6d7dd71f2fa57
Tiago Almeida <103502300+Tigas14@users.noreply.github.com>
2023-02-06 17:28:54 +0000

    editar perfil

--- backoffice/src/Pages/Perfil/editarPerfil.js
+++ backoffice/src/Pages/Perfil/editarPerfil.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import auth from "../../Auth/auth.service";
 
 import FormEditar from '../../Components/FormEditar';
+
+const ip = process.env.REACT_APP_IP;
 
 export default function EditarPerfil() {
   const [nome, setNome] = useState('')
@@ -8,11 +12,26 @@ export default function EditarPerfil() {
   const [genero, setGenero] = useState('')
   const [localidade, setLocalidade] = useState('')
 
-  useEffect(() => {
-    setGenero("Masculino")
-    console.log(nome)
-  }, [nome])
+  const [utilizadores, setUtilizadores] = useState([]);
 
+  useEffect(() => {
+    axiosGetUtilizador()
+  }, [])
+
+  async function axiosGetUtilizador() {
+    const url = ip + "/utilizador/" + auth.getUser().id;
+
+    await axios
+      .get(url, auth.header())
+      .then((output) => {
+        console.log(output.data.data);
+        setUtilizadores(output.data?.data ?? []);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+/*
   const teste = [{
     nome: "Nome",
     id: "inputNome",
@@ -34,14 +53,14 @@ export default function EditarPerfil() {
     value: localidade,
     setState: setLocalidade
   }]
-
+*/
   return (
     <>
       <div className="container-fluid">
         <div className="row">
 
           <div className="col-12 offset-md-2 col-md-8">
-            <FormEditar itens={teste} onClick={() => console.log("click")}/>
+            <FormEditar itens={utilizadores} onClick={() => console.log("click")}/>
           </div>
 
         </div>
